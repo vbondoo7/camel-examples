da11c542c42152d608fc7e19b0bc1a329e69390c
Vishal Saxena <Vishal.Saxena@officedepot.com>
2019-03-12 16:28:47 +0530

    updated exception handling for outbound

--- src/main/java/com/od/eai/services/ctusoap/exception/handler/ExceptionMessageHandler.java
+++ src/main/java/com/od/eai/services/ctusoap/exception/handler/ExceptionMessageHandler.java
@@ -5,9 +5,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import com.officedepot.eai.service.translationutility.BulkTranslationUpsertResponseType;
 import com.officedepot.eai.service.translationutility.ResultType;
 import com.officedepot.eai.service.translationutility.StatusType;
 import com.officedepot.eai.service.translationutility.TranslationLookupResponseType;
+import com.officedepot.eai.service.translationutility.TranslationUpsertResponseType;
 
 @Component
 public class ExceptionMessageHandler {
@@ -17,15 +19,34 @@ public class ExceptionMessageHandler {
 	public void handleFallback(Exchange exchange) throws Exception {
 		if (exchange.getProperty(Exchange.EXCEPTION_CAUGHT) != null) {
 			Exception exception = exchange.getProperty(Exchange.EXCEPTION_CAUGHT, Exception.class);
-			LOGGER.error("An Exception has been thrown by CTU Soap service: {}", exception.getMessage()==null?exception.toString():exception.getMessage());
-			TranslationLookupResponseType translationLookupResponseType = new TranslationLookupResponseType();
-			StatusType statusType = new StatusType();
-			ResultType result = new ResultType();
-			statusType.setStatus("error");
-			statusType.setStatusCode(1);
-			statusType.setStatusDescription("connection error with CTU Lookup Services");
-			result.setStatus(statusType);
-			translationLookupResponseType.setResult(result);
+			String operationName = (String) exchange.getProperty("operationName");
+			LOGGER.error("An Exception has been thrown by CTU Soap service for operation {} : {}", operationName, exception.getMessage()==null?exception.toString():exception.getMessage());
+			if(operationName.trim().equalsIgnoreCase("translationLookUpRequest")) {
+				TranslationLookupResponseType translationLookupResponseType = new TranslationLookupResponseType();
+				StatusType statusType = new StatusType();
+				ResultType result = new ResultType();
+				statusType.setStatus("error");
+				statusType.setStatusCode(1);
+				statusType.setStatusDescription("connection error with CTU Lookup Services");
+				result.setStatus(statusType);
+				translationLookupResponseType.setResult(result);
+				exchange.getOut().setBody(translationLookupResponseType);
+			}
+			else if(operationName.trim().equalsIgnoreCase("bulkTranslationLookUpRequest")) {
+				//TODO
+			}
+			else if(operationName.trim().equalsIgnoreCase("translationUpsertRequest")) {
+				TranslationUpsertResponseType translationUpsertResponse = new TranslationUpsertResponseType();
+				translationUpsertResponse.setStatus(getStatusTypeWithFailure(exception.getMessage()));
+				exchange.getOut().setBody(translationUpsertResponse);
+			}
+			else if(operationName.trim().equalsIgnoreCase("bulkTranslationUpsertRequest")) {
+				BulkTranslationUpsertResponseType bulkTranslationUpsertResponse = new BulkTranslationUpsertResponseType();
+				bulkTranslationUpsertResponse.setStatus(getStatusTypeWithFailure(exception.getMessage()));
+				exchange.getOut().setBody(bulkTranslationUpsertResponse);
+			}
+			
+			exchange.getOut().setHeader(Exchange.HTTP_RESPONSE_CODE, 500);
 		}
 	}
 	
@@ -44,4 +65,11 @@ public class ExceptionMessageHandler {
 		}
 	}
 
+	private StatusType getStatusTypeWithFailure(String errorMessage) {
+		StatusType statusType = new StatusType();
+		statusType.setStatus("ERROR");
+		statusType.setStatusCode(-1);
+		statusType.setStatusDescription(errorMessage);
+		return statusType;
+	}
 }
